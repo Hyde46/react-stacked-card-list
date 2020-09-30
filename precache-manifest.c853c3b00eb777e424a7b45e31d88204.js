7d4de25d72fc0a7758df176aea0063fc0e715743
Hyde46 <denis.heid@protonmail.com>
2020-09-30 23:09:51 +0200

    Updates

---
+++ precache-manifest.c853c3b00eb777e424a7b45e31d88204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f84b519a3f134047a5ec19ed88bc0a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01a1ec8a4d7cc0448bdf",
+    "url": "./static/css/main.d1854546.chunk.css"
+  },
+  {
+    "revision": "ef76081b21876ed54e77",
+    "url": "./static/js/2.a8813077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a8813077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a1ec8a4d7cc0448bdf",
+    "url": "./static/js/main.b1f69e07.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.b1f69e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c417bc79a650dee5ac",
+    "url": "./static/js/runtime-main.b24facc9.js"
+  }
+]);
